02e5869d0409185318eb8abffdb18cdc47039b2e
novichkov <novichkov@smartsolutions-123.ru>
2022-11-01 09:29:58 +0400

    Add case for orders

--- tests/test_full_path.py
+++ tests/test_full_path.py
@@ -11,6 +11,15 @@ class FullPathTest(unittest.TestCase):
         full_path = get_full_plan_path(plan, couriers)
         self.assertEqual(full_path, 15)
 
+    def test_one_courier_two_orders(self):
+        courier = Courier('Courier1', Point(3, 4))
+        couriers = [courier]
+        order_1 = Order('Order1', 6, 8, 0)
+        order_2 = Order('Order2', 1, 0, 0)
+        plan = {courier.name: [order_1, order_2]}
+        full_path = get_full_plan_path(plan, couriers)
+        self.assertEqual(full_path, 17)
+
 
 if __name__ == '__main__':
     unittest.main()
