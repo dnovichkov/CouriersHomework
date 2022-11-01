09468bf62ee1f5a20c26e00dac76b9babb568723
Dmitriy Novichkov <dmitriy.novichkov@gmail.com>
2022-11-01 06:33:44 +0400

    Add classes

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 import math
 
 import plotly.graph_objects as go
-import streamlit as st
+# import streamlit as st
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_distance_to_other(self, other_point):
+        order_distance = math.dist((self.x, self.y), (other_point.x, other_point.y))
+        return order_distance
+
+    def get_distance_to_center(self):
+        return self.get_distance_to_other(Point(0, 0))
+
+
+class Courier:
+    def __init__(self, name: str, point: Point):
+        self.name = name
+        self.point = point
+
+    def __str__(self):
+        return self.name
+
+
+class Order:
+    def __init__(self, name: str, x, y, price=0):
+        self.name = name
+        self.point = Point(x, y)
+        self.price = price
+
+    def __repr__(self):
+        return self.name
 
 
 def load_couriers():
+    # Можно читать из файла
     _couriers = \
         [
             ("Курьер 1", 2, 2),
@@ -12,7 +45,8 @@ def load_couriers():
             ("Курьер 3", 5, 5),
             ("Курьер 4", 21, 21),
         ]
-    return _couriers
+
+    return [Courier(_c[0], Point(_c[1], _c[2])) for _c in _couriers]
 
 
 def load_orders():
@@ -23,7 +57,8 @@ def load_orders():
             ("C", 8, 5),
             ("D", 6, 6),
         ]
-    return _orders
+
+    return [Order(order[0], order[1], order[2], 0) for order in _orders]
 
 
 def show_plot(orders, couriers):
@@ -89,9 +124,7 @@ def calculate_plan(couriers, orders):
         print(f'Запустили итерацию {i}')
         for order in orders:
             print(f'Ищем курьера для заказа {order}')
-            order_x = order[1]
-            order_y = order[2]
-            order_distance = math.dist((order_x, order_y), (0, 0))
+            order_distance = order.point.get_distance_to_center()
             print(f'Расстояние до центра составляет {order_distance}')
 
             possible_courier = None
@@ -100,9 +133,7 @@ def calculate_plan(couriers, orders):
             for courier in couriers:
                 print(f'Пробуем курьера {courier}')
 
-                courier_x = courier[1]
-                courier_y = courier[2]
-                courier_distance = math.dist((courier_x, courier_y), (order_x, order_y))
+                courier_distance = courier.point.get_distance_to_other(order.point)
 
                 print(f'Суммарное расстояние до заказа составляет {courier_distance}')
 
@@ -112,9 +143,9 @@ def calculate_plan(couriers, orders):
                     possible_courier = courier
                     min_full_distance = fill_distance
             print(f'Для заказа {order} лучший курьер - {possible_courier}')
-            if possible_courier[0] not in couriers_plan:
-                couriers_plan[possible_courier[0]] = []
-            couriers_plan[possible_courier[0]].append(order)
+            if possible_courier and possible_courier.name not in couriers_plan:
+                couriers_plan[possible_courier.name] = []
+            couriers_plan[possible_courier.name].append(order)
     return couriers_plan
 
 
@@ -133,9 +164,7 @@ def calculate_plan_using_full_path_couriers_orders(couriers, orders):
 
         for order in orders:
 
-            order_x = order[1]
-            order_y = order[2]
-            order_distance = math.dist((order_x, order_y), (0, 0))
+            order_distance = order.point.get_distance_to_center()
             print(f'Расстояние до центра составляет {order_distance}')
 
             possible_courier = None
@@ -144,11 +173,8 @@ def calculate_plan_using_full_path_couriers_orders(couriers, orders):
 
             print(f'Пробуем курьера {courier}')
             print(f'Ищем курьера для заказа {order}')
-            courier_name = courier[0]
-
-            courier_x = courier[1]
-            courier_y = courier[2]
-            courier_distance = math.dist((courier_x, courier_y), (order_x, order_y))
+            courier_name = courier.name
+            courier_distance = courier.point.get_distance_to_other(order.point)
 
             if courier_name in couriers_plan:
                 # Курьер уже выполнил какой-то заказ и теперь находится в точке (0;0),
@@ -159,15 +185,14 @@ def calculate_plan_using_full_path_couriers_orders(couriers, orders):
 
                 # Надо посмотреть, какой путь он уже проделал
                 for order_number, visited_order in enumerate(couriers_orders):
-                    visited_order_distance = math.dist((visited_order[1], visited_order[2]), (0, 0))
+                    visited_order_distance = visited_order.point.get_distance_to_center()
 
                     # Добавляем в пройденный путь то, как мы шли с заказом
                     courier_distance += visited_order_distance
 
                     if order_number == 0:
                         # За первым заказом мы ходили из начальной точки курьера
-                        courier_distance += math.dist((courier_x, courier_y),
-                                                         (visited_order[1], visited_order[2]))
+                        courier_distance += courier.point.get_distance_to_other(visited_order.point)
                     else:
                         # За остальными заказами мы ходили из точки (0;0
                         courier_distance += visited_order_distance
@@ -180,9 +205,9 @@ def calculate_plan_using_full_path_couriers_orders(couriers, orders):
                 possible_courier = courier
                 min_full_distance = fill_distance
         print(f'Для заказа {order} лучший курьер - {possible_courier} с расстоянием {min_full_distance}')
-        if possible_courier[0] not in couriers_plan:
-            couriers_plan[possible_courier[0]] = []
-        couriers_plan[possible_courier[0]].append(order)
+        if possible_courier.name not in couriers_plan:
+            couriers_plan[possible_courier.name] = []
+        couriers_plan[possible_courier.name].append(order)
     return couriers_plan
 
 
@@ -198,9 +223,7 @@ def calculate_plan_using_full_path_order_couriers(couriers, orders):
     for order in orders:
         print(f'Ищем курьера для заказа {order}')
 
-        order_x = order[1]
-        order_y = order[2]
-        order_distance = math.dist((order_x, order_y), (0, 0))
+        order_distance = order.point.get_distance_to_center()
         print(f'Расстояние до центра составляет {order_distance}')
 
         possible_courier = None
@@ -208,11 +231,8 @@ def calculate_plan_using_full_path_order_couriers(couriers, orders):
         min_full_distance = 0
         for courier in couriers:
             print(f'Пробуем курьера {courier}')
-            courier_name = courier[0]
-
-            courier_x = courier[1]
-            courier_y = courier[2]
-            courier_distance = math.dist((courier_x, courier_y), (order_x, order_y))
+            courier_name = courier.name
+            courier_distance = courier.point.get_distance_to_other(order.point)
 
             if courier_name in couriers_plan:
                 # Курьер уже выполнил какой-то заказ и теперь находится в точке (0;0),
@@ -223,15 +243,14 @@ def calculate_plan_using_full_path_order_couriers(couriers, orders):
 
                 # Надо посмотреть, какой путь он уже проделал
                 for order_number, visited_order in enumerate(couriers_orders):
-                    visited_order_distance = math.dist((visited_order[1], visited_order[2]), (0, 0))
+                    visited_order_distance = visited_order.point.get_distance_to_center()
 
                     # Добавляем в пройденный путь то, как мы шли с заказом
                     courier_distance += visited_order_distance
 
                     if order_number == 0:
                         # За первым заказом мы ходили из начальной точки курьера
-                        courier_distance += math.dist((courier_x, courier_y),
-                                                         (visited_order[1], visited_order[2]))
+                        courier_distance += courier.point.get_distance_to_other(visited_order.point)
                     else:
                         # За остальными заказами мы ходили из точки (0;0
                         courier_distance += visited_order_distance
@@ -244,31 +263,28 @@ def calculate_plan_using_full_path_order_couriers(couriers, orders):
                 possible_courier = courier
                 min_full_distance = fill_distance
         print(f'Для заказа {order} лучший курьер - {possible_courier} с расстоянием {min_full_distance}')
-        if possible_courier[0] not in couriers_plan:
-            couriers_plan[possible_courier[0]] = []
-        couriers_plan[possible_courier[0]].append(order)
+        if possible_courier.name not in couriers_plan:
+            couriers_plan[possible_courier.name] = []
+        couriers_plan[possible_courier.name].append(order)
     return couriers_plan
 
 
 def get_full_plan_path(plan, couriers):
     full_path = 0
     for courier in couriers:
-        name = courier[0]
-        courier_x = courier[1]
-        courier_y = courier[2]
+        name = courier.name
         if name not in plan:
             continue
         for number, order in enumerate(plan[name]):
             # Добавляем в пройденный путь то, как мы шли с заказом
-            full_path += math.dist((order[1], order[2]), (0, 0))
+            full_path += order.point.get_distance_to_center()
 
             if number == 0:
                 # За первым заказом мы ходили из начальной точки курьера
-                full_path += math.dist((courier_x, courier_y),
-                                                    (order[1], order[2]))
+                full_path += courier.point.get_distance_to_other(order.point)
             else:
                 # За остальными заказами мы ходили из точки (0;0
-                full_path += math.dist((order[1], order[2]), (0, 0))
+                full_path += order.point.get_distance_to_center()
 
     return full_path
 
@@ -276,7 +292,7 @@ def get_full_plan_path(plan, couriers):
 def main():
     couriers = load_couriers()
     orders = load_orders()
-    show_plot(orders, couriers)
+    # show_plot(orders, couriers)
 
     # Ключ - имя курьера, значение - его заказы
     couriers_plan = calculate_plan(couriers, orders)
@@ -285,7 +301,7 @@ def main():
     plan_distance = get_full_plan_path(couriers_plan, couriers)
     print(f'{plan_distance=}')
 
-    expected_plan = {'Курьер 1': [('A', 1, 1), ('B', 3, 3)], 'Курьер 2': [('C', 8, 5), ('D', 6, 6)]}
+    expected_plan = {'Курьер 1': [Order('A', 1, 1), Order('B', 3, 3)], 'Курьер 2': [Order('C', 8, 5), Order('D', 6, 6)]}
     print(expected_plan == couriers_plan)
 
     print("Запускаем расчет плана с историей перемещений")
@@ -301,7 +317,7 @@ def main():
 
     print("Запускаем расчет плана с историей перемещений - сначала курьеры")
     couriers_plan_using_full_path_couriers_orders = calculate_plan_using_full_path_couriers_orders(couriers, orders)
-    best_plan = {'Курьер 1': [('A', 1, 1), ('D', 6, 6)], 'Курьер 3': [('B', 3, 3)], 'Курьер 2': [('C', 8, 5)]}
+    best_plan = {'Курьер 1': [Order('A', 1, 1), Order('D', 6, 6)], 'Курьер 3': [Order('B', 3, 3)], 'Курьер 2': [Order('C', 8, 5)]}
     print(f'{couriers_plan_using_full_path_couriers_orders=}')
     print(expected_plan == couriers_plan_using_full_path_couriers_orders)
     print(best_plan == couriers_plan_using_full_path_order_couriers)
@@ -311,9 +327,9 @@ def main():
     print(f'{plan_distance=}')
 
     possible_best_plan = {
-        'Курьер 1': [('B', 3, 3), ('A', 1, 1)],
-        'Курьер 2': [('C', 8, 5)],
-        'Курьер 3': [('D', 6, 6)],
+        'Курьер 1': [Order('B', 3, 3), Order('A', 1, 1)],
+        'Курьер 2': [Order('C', 8, 5)],
+        'Курьер 3': [Order('D', 6, 6)],
     }
     possible_best_plan_distance = get_full_plan_path(possible_best_plan, couriers)
     print(f'{possible_best_plan_distance=}')
